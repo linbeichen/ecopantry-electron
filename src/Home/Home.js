c5fabf0b6631db4f1fa2c8ad9d959e15053c57b2
Beichen Lin <blin0035@student.monash.edu>
2025-01-11 21:11:14 +1100

    home page complete

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "./HeroSection";
 import Introduction from "./Introduction";
+import Features from "./Feature";
 
 const Home = () => {
     return (
         <div>
             <HeroSection />
             <Introduction />
+            <Features />
         </div>
     )
 };
