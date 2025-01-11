6502449eabb5eadbaa7f6100c70f09ec01a83e36
Beichen Lin <blin0035@student.monash.edu>
2025-01-11 21:05:12 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Home from './Home/Home';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "./HeroSection";
+import Introduction from "./Introduction";
+
+const Home = () => {
+    return (
+        <div>
+            <HeroSection />
+            <Introduction />
+        </div>
+    )
+};
+
+export default Home;
